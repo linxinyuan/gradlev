e6811dacb285b75f0175cf1ac9e21e672d93e3f1
linxinyuan <linxinyuan@lizhi.fm>
2018-12-21 01:32:07 +0800

    [update]脚本修正

--- ginit.sh
+++ ginit.sh
@@ -2,3 +2,4 @@
 echo "export PATH=$(pwd)/:\$PATH" >~/.bash_profile
 echo "$(pwd)/tools/buildServer.sh" >gradlev
 source ~/.bash_profile
+dos2unix ./tools/buildServer.sh ./tools/loginssh.sh ./tools/scp.sh ./tools/spawnssh.sh
